f66fea3b376c9992211209ace18786f46718d98b
antekpiechnik <antek.piechnik@gmail.com>
2010-08-13 01:02:08 +0200

    Rewrote the ActiveRecord queries to match the 3.0 API

--- lib/navvy/job/active_record.rb
+++ lib/navvy/job/active_record.rb
@@ -10,7 +10,7 @@ module Navvy
     # @param [Symbol, String] method_name the name of the method you want to run
     # @param [*] arguments optional arguments you want to pass to the method
     #
-    # @return [true, false]
+    # @return [Job, false]
 
     def self.enqueue(object, method_name, *args)
       options = {}
@@ -42,14 +42,11 @@ module Navvy
     # jobs were found.
 
     def self.next(limit = self.limit)
-      all(
-        :conditions => [
-          "#{connection.quote_column_name('failed_at')} IS NULL AND #{connection.quote_column_name('completed_at')} IS NULL AND #{connection.quote_column_name('run_at')} <= ?",
-          Time.now
-        ],
-        :limit =>         limit,
-        :order =>         'priority desc, created_at'
-      )
+      where(:failed_at => nil).
+        where(:completed_at => nil).
+        where(["run_at <= ?", Time.now]).
+        order("priority DESC, created_at").
+        limit(limit)
     end
 
     ##
@@ -61,14 +58,9 @@ module Navvy
 
     def self.cleanup
       if keep.is_a? Fixnum
-        delete_all([
-          "#{connection.quote_column_name('completed_at')} <= ?",
-          keep.ago
-        ])
+        where(["completed_at <= ?", keep.ago]).delete_all
       else
-        delete_all(
-          "#{connection.quote_column_name('completed_at')} IS NOT NULL"
-        ) unless keep?
+        where("completed_at IS NOT NULL").delete_all unless keep?
       end
     end
 
@@ -79,9 +71,7 @@ module Navvy
     # update_attributes call
 
     def started
-      update_attributes({
-        :started_at =>  Time.now
-      })
+      update_attributes(:started_at =>  Time.now)
     end
 
     ##
@@ -94,10 +84,10 @@ module Navvy
     # update_attributes call
 
     def completed(return_value = nil)
-      update_attributes({
+      update_attributes(
         :completed_at =>  Time.now,
         :return =>        return_value
-      })
+      )
     end
 
     ##
@@ -126,9 +116,9 @@ module Navvy
 
     def times_failed
       i = parent_id || id
-      self.class.count(
-        :conditions => "(#{connection.quote_column_name('id')} = '#{i}' OR #{connection.quote_column_name('parent_id')} = '#{i}') AND #{connection.quote_column_name('failed_at')} IS NOT NULL"
-      )
+      self.class.where("failed_at IS NOT NULL").
+        where(["id = ? OR parent_id = ?", i, i]).
+        count
     end
   end
 end
